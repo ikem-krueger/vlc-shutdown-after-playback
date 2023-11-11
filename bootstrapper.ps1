4abcc86bc3ab9167fcc78d0d9b2dfd0e8780e5ca
Ikem Krueger <ikem.krueger@gmail.com>
2023-11-11 16:37:26 +0100

    Refactor bootstrapper.ps1

--- bootstrapper.ps1
+++ bootstrapper.ps1
@@ -5,7 +5,7 @@ Add-Type -TypeDefinition @"
 
     public class TrayTip {
         public static void Show(string title, string text, int timeout) {
-            NotifyIcon trayIcon = new NotifyIcon();
+            NotIfyIcon trayIcon = new NotIfyIcon();
             trayIcon.Visible = true;
             trayIcon.Icon = SystemIcons.Information;
             trayIcon.BalloonTipTitle = title;
@@ -31,14 +31,20 @@ public class NativeFunctions
 }
 "@
 
-$TIMEOUT = 20 # seconds
+Function Show-MessageBox {
+    $TIMEOUT = 20 # Seconds
 
-$MB_ICONINFORMATION = 0x40
-$MB_YESNO = 0x4
-$MB_TITLE = "Standby in progress"
-$MB_TEXT = "This PC is going into standby in $TIMEOUT seconds.`n`nDo you want to abort this action?"
+    $MB_ICONINFORMATION = 0x40
+    $MB_YESNO = 0x4
+    $MB_TITLE = "Standby in progress"
+    $MB_TEXT = "This PC is going into standby in $TIMEOUT seconds.`n`nDo you want to abort this action?"
 
-$IDYES = 6
+    Return [NativeFunctions]::MessageBoxTimeout([NativeFunctions]::GetForegroundWindow(), $MB_TEXT, $MB_TITLE, $MB_ICONINFORMATION -bor $MB_YESNO, 0, $TIMEOUT * 1000)
+}
+
+Function Close-Player {
+    Stop-Process -Name "vlc" -Force
+}
 
 Function Trigger-Standby {
   Add-Type -Assembly System.Windows.Forms
@@ -49,47 +55,48 @@ Function Trigger-Standby {
 }
 
 Function Main {
-    $vlcProcess = $Null
+    $vlcProcess = $null
 
     $previousState = ""
     $actualState = ""
 
-    While($true) {
+    While ($true) {
         Start-Sleep -Milliseconds 1000
 
         try {
             # Running
             $vlcProcess = Get-Process -Name "vlc" -ErrorAction Stop
 
-            If($vlcProcess.mainWindowTitle -match ".+VLC media player") {
+            If ($vlcProcess.mainWindowTitle -match ".+VLC media player") {
                 $actualState = "Playing"
             }
 
-            If($vlcProcess.mainWindowTitle -eq "VLC media player") {
+            If ($vlcProcess.mainWindowTitle -eq "VLC media player") {
                 $actualState = "Idle"
             }
         } catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
             # Closed
-            break
+            Break
         }
 
-        If($previousState -eq "Playing" -and $actualState -eq "Idle") {
-            $choice = [NativeFunctions]::MessageBoxTimeout([NativeFunctions]::GetForegroundWindow(), $MB_TEXT, $MB_TITLE, $MB_ICONINFORMATION -bor $MB_YESNO, 0, $TIMEOUT * 1000)
+        If ($previousState -eq "Playing" -and $actualState -eq "Idle") {
+            $choice = Show-MessageBox
 
-            If ($choice -eq $IDYES) {
-                break
+            # "If user choose 'Yes'..."
+            If ($choice -eq 6) {
+                Break
             }
 
-            Write-Host "Close VLC..."
+            Write-Host "Close player..."
 
-            Stop-Process -Name "vlc" -Force
+            Close-Player
 
             Write-Host "Trigger standby..."
 
             Trigger-Standby
         }
 
-        If($previousState -ne $actualState) {
+        If ($previousState -ne $actualState) {
             Write-Host "State: $actualState"
 
             $previousState = $actualState
